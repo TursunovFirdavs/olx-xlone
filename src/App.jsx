333212b8a4d6df8f4293a328f020ccd5e2a090f4
TursunovFirdavs <tursunovfirdavs571@gmail.com>
2024-03-15 15:14:44 +0500

    get full date

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import { useEffect, useState } from 'react'
 import { Navbar } from './components'
 import Footer from './components/Footer'
 import Pages from './pages'
 
 function App() {
-
+  
   return (
     <div>
       <Navbar/>
